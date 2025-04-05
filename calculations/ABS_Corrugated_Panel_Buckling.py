1f04b92941cb62bc8478c6e4a55aacb798897e20
amolnwagh <waghamoln02@gmail.com>
2025-04-05 17:11:43 +0530

    Added functions for entire corrugated bulkhead checks

--- calculations/ABS_Corrugated_Panel_Buckling.py
+++ calculations/ABS_Corrugated_Panel_Buckling.py
@@ -25,9 +25,9 @@ class Corr_Panel(ABS.Panel):
     phi: float # angle of web of corrugated panel with respect to flange (degrees)
     sigma_a_unit_c: float # maximum compressive stress in unit corrugation (psi)
     sigma_b_unit_c: float # maximum bending compressive stress in unit corrugation due to hydrostatic pressure (psi)
-    sigma_x_panel: float # calculated average compressive stress in the corrugation direction for entire corrugated panel (psi)
-    sigma_y_panel: float # calculated average compressive stress in the transverse direction for entire corrugated panel (psi)
-    tau_panel_panel: float # in-plane shear stress for entire corrugated panel (psi)
+    sigma_x_cbhd: float # calculated average compressive stress in the corrugation direction for entire corrugated panel (bulkhead) (psi)
+    sigma_y_cbhd: float # calculated average compressive stress in the transverse direction for entire corrugated panel (bulkhead) (psi)
+    tau_cbhd: float # in-plane shear stress for entire corrugated panel (bulkhead) (psi)
 
     def d(self):
         return calc_d(self.c, self.phi)    
@@ -81,13 +81,10 @@ class Corr_Panel(ABS.Panel):
             self.eta()
         )
         
-    
-    
-    
-    
-    
 
+#+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---
 
+    
 def calc_spc(a:float, b:float, c:float, phi:float) -> float:
     """calculates spacing of unit corrugation
 
@@ -119,7 +116,7 @@ def calc_d(c:float, phi:float) -> float:
 
 
 def calc_tx(spc:float, t:float, Asx:float) -> float:
-    """calculates section property "tx" of the corrugated panel
+    """Calculates equivalent thickness of the corrugation in the corrugation direction
 
     Args:
         spc (float): spacing of unit corrugation (in)
@@ -127,7 +124,7 @@ def calc_tx(spc:float, t:float, Asx:float) -> float:
         Asx (float): section property "Asx" of the corrugated panel (in^2)
 
     Returns:
-        tx (float): section property "tx" of the corrugated panel
+        tx (float): equivalent thickness of the corrugation in the corrugation direction (in)
     """
     tx = (spc*t + Asx)/spc
     return tx
@@ -256,7 +253,7 @@ def calc_sigma_EC(E:float, r:float, L:float) -> float:
         sigma_EC (float): Elastic buckling stress for unit corrugation (psi)
     """
 
-    sigma_EC = (pi()**2)*E*(r**2)/(L**2)
+    sigma_EC = (pi**2)*E*(r**2)/(L**2)
     return sigma_EC
 
 
@@ -268,7 +265,7 @@ def calc_sigma_EB(kc:float, t:float, a:float, b:float, E:float=30.0e6, nu:float=
         t (float): thickness of plating (in)
         a (float): width of flange panel "a" (in)
         b (float): width of flange panel "b" (in)
-        E (floatn,optional): modulus of elasticity. Defaults to 30.0e6, psi .
+        E (float,optional): modulus of elasticity. Defaults to 30.0e6, psi.
         nu (float,optional): Poisson's ratio. Defaults to 0.3 for steel.
 
     Returns:
@@ -292,12 +289,194 @@ def calc_UC_unit_corr(sigma_a:float, sigma_b:float, sigma_CA:float, sigma_CB:flo
         eta (float): maximum allowable strength factor, eta
 
     Returns:
-        UC_unit_corr (float): Utilization ratio for beam-column buckling of unit corrugation 
+        UC_unit_corr (float): Unity Check ratio for beam-column buckling of unit corrugation 
         under compressive stress and lateral hydrostatic pressure
     """
     Cm = 1.0 # Bending moment factor considered for bensing stresses taken directly from Finite ELement Analysis
     UC_unit_corr = (sigma_a/(eta*sigma_CA)) + ((Cm*sigma_b)/(eta*sigma_CB*(1-(sigma_a/(eta*sigma_EC)))))
     return UC_unit_corr
+
+
+def calc_Dx(Iy:float, spc:float, E:float = 30.0e6) -> float:
+    """Calculates Dx
+
+    Args:
+        Iy (float): moment of inertia of unit corrugation about its weak axis (in^4)
+        spc (float): spacing of unit corrugation (in)
+        E (float,optional): modulus of elasticity. Defaults to 30.0e6, psi.
+
+    Returns:
+        Dx (float)
+    """
+    Dx = E*Iy/spc
+    return Dx
+
+
+def calc_Dy(a:float, b:float, c:float, t:float, spc:float, E:float, nu:float) -> float:
+    """Calculates Dy
+
+    Args:
+        a (float): width of flange panel "a" (in)
+        b (float): width of flange panel "b" (in)
+        c (float): inclined width of web panel "c" (in)
+        t (float): thickness of plating (in)
+        spc (float): spacing of unit corrugation (in)
+        E (float,optional): modulus of elasticity. Defaults to 30.0e6, psi.
+        nu (float, optional): Poisson's ratio. Defaults to 0.3 for steel.
+
+    Returns:
+        Dy (float)
+    """
+    Dy = ((E*(t**3))/(12*(1-(nu**2))))*(spc/(a+b+2*c))
+    return Dy
+
+
+def calc_phi_x(L:float, B:float, Dx:float, Dy:float) -> float:
+    """Calculates phi_x
+
+    Args:
+        L (float): Length of entire corrugated panel (bulkhead) (in) 
+        B (float): Width of entire corrugated panel (bulkhead) (in)
+        Dx (float)
+        Dy (float)
+    
+    Returns:
+        phi_x (float)
+    """
+    phi_x = (L/B) * (Dy/Dx)**(1/4)
+    return phi_x
+
+
+def calc_phi_y(L:float, B:float, Dx:float, Dy:float) -> float:
+    """Calculates phi_y
+
+    Args:
+        L (float): Length of entire corrugated panel (bulkhead) (in) 
+        B (float): Width of entire corrugated panel (bulkhead) (in)
+        Dx (float)
+        Dy (float)
+    
+    Returns:
+        phi_y (float)
+    """
+    phi_y = (B/L) * (Dx/Dy)**(1/4)
+    return phi_y
+
+
+def calc_kx_cbhd(L:float, B:float, Dx:float, Dy:float, phi_x:float) -> float:
+    """Calculates kx for entire corrugated bulkhead
+
+    Args:
+        L (float): Length of entire corrugated panel (bulkhead) (in) 
+        B (float): Width of entire corrugated panel (bulkhead) (in)
+        Dx (float)
+        Dy (float)
+        phi_x (float)
+    
+    Returns:
+        kx_cbhd (float): kx for entire corrugated bulkhead
+    """
+    if (L/B) >= 0.5176 * (Dx/Dy)**(1/4):
+        kx_cbhd = 4
+    else:
+        kx_cbhd = (1/(phi_x**2)) + phi_x**2  
+    return kx_cbhd
+
+
+def calc_ky_cbhd(L:float, B:float, Dx:float, Dy:float, phi_y:float) -> float:
+    """Calculates ky for entire corrugated bulkhead
+
+    Args:
+        L (float): Length of entire corrugated panel (bulkhead) (in) 
+        B (float): Width of entire corrugated panel (bulkhead) (in)
+        Dx (float)
+        Dy (float)
+        phi_y (float)
+    
+    Returns:
+        kx_cbhd (float): ky for entire corrugated bulkhead
+    """
+    if (B/L) >= 0.5176 * (Dy/Dx)**(1/4):
+        ky_cbhd = 4
+    else:
+        ky_cbhd = (1/(phi_y**2)) + phi_y**2    
+    return ky_cbhd
+
+
+def calc_ks_cbhd() -> float:
+    """Calculates ks for entire corrugated bulkhead
+
+    Returns:
+        ks (float): A constant value
+    """
+    ks_cbhd = 3.65
+    return ks_cbhd
+
+def calc_sigma_Ex_cbhd(kx:float, Dx:float, Dy:float, tx:float, B:float) -> float:
+    """Calculates elastic buckling stress in the corrugation direction for entire corrugated panel (bulkhead)
+
+    Args:
+        kx (float)
+        Dx (float)
+        Dy (float)
+        tx (float): equivalent thickness of the corrugation in the corrugation direction (in)
+        B (float): Width of entire corrugated panel (bulkhead) (in)
+
+    Returns:
+        float: elastic buckling stress in the corrugation direction for entire corrugated panel (bulkhead) (psi)
+    """
+    sigma_Ex_cbhd = kx * (pi**2) * (Dx*Dy)**(1/2) / (tx*(B**2))
+    return sigma_Ex_cbhd
+
+def calc_sigma_Ey_cbhd(ky:float, Dx:float, Dy:float, t:float, L:float) -> float:
+    """Calculates elastic buckling stress in the transverse direction for entire corrugated panel (bulkhead)
+
+    Args:
+        ky (float)
+        Dx (float)
+        Dy (float)
+        t (float): thickness of plating (in)
+        B (float): Length of entire corrugated panel (bulkhead) (in)
+
+    Returns:
+        float: elastic buckling stress in the transverse direction for entire corrugated panel (bulkhead) (psi)
+    """
+    sigma_Ey_cbhd = ky * (pi**2) * (Dx*Dy)**(1/2) / (t*(L**2))
+    return sigma_Ey_cbhd
+
+def calc_tau_E_cbhd(ks:float, Dx:float, Dy:float, t:float, L:float) -> float:
+    """Calculates elastic shear buckling stress for entire corrugated panel (bulkhead)
+
+    Args:
+        ks (float)
+        Dx (float)
+        Dy (float)
+        t (float): thickness of plating (in)
+        B (float): Length of entire corrugated panel (bulkhead) (in)
+
+    Returns:
+        float: elastic shear buckling stress for entire corrugated panel (bulkhead) (psi)
+    """
+    tau_E_cbhd = ks * (pi**2) * (Dx**(3/4))*(Dy**(1/4)) / (t*(L**2))
+    return tau_E_cbhd
+
+def calc_UC_cbhd(sigma_x: float, sigma_y: float, tau: float, sigma_Gx: float, sigma_Gy: float, tau_G: float, eta: float) -> float:
+    """_summary_
+
+    Args:
+        sigma_x (float): calculated average compressive stress in the corrugation direction for entire corrugated panel (bulkhead) (psi)
+        sigma_y (float): calculated average compressive stress in the transverse direction for entire corrugated panel (bulkhead) (psi)
+        tau (float): in-plane shear stress for entire corrugated panel (bulkhead) (psi)
+        sigma_Gx (float): critical buckling stress for uniaxial compression in the corrugation direction (psi)
+        sigma_Gy (float): critical buckling stress for uniaxial compression in the transverse direction (psi)
+        tau_G (float): critical buckling stress for shear stress (psi)
+        eta (float): maximum allowable strength factor, eta
+    
+    Returns:
+        UC_cbhd (float): Unity check ratio for entire corrugated panel (bulkhead)
+    """
+    UC_cbhd = (sigma_x/(eta*sigma_Gx))**2 + (sigma_y/(eta*sigma_Gy))**2 + (tau/(eta*tau_G))**2
+    return UC_cbhd
 
 
     
