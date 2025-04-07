4f9a7de21e10099b05b8c765c6254185d80c399b
amolnwagh <waghamoln02@gmail.com>
2025-04-07 16:16:50 +0530

    writing tests

--- calculations/test_ABS_Corrugated_Panel_Buckling.py
+++ calculations/test_ABS_Corrugated_Panel_Buckling.py
@@ -9,70 +9,66 @@ def test_calc_d():
     assert math.isclose(ABScorr.calc_d(18,45),12.727922061357857)
     assert math.isclose(ABScorr.calc_d(12,52),9.456129043280665)
     
-def calc_tx():
+def test_calc_tx():
     assert math.isclose(ABScorr.calc_tx(72,0.5,15),0.7083333333333334)
     assert math.isclose(ABScorr.calc_tx(80,0.75,15),0.9375)
 
-def calc_A():
+def test_calc_A():
     assert math.isclose(ABScorr.calc_A(24,28,18,0.5),44.0)
     assert math.isclose(ABScorr.calc_A(24,24,12,0.75),54.0)
+    
+def test_calc_Asx():
+    assert math.isclose(ABScorr.calc_Asx(18,0.5,45),12.727922061357857)
+    assert math.isclose(ABScorr.calc_Asx(12,0.75,52),14.184193564920996)
 
+def test_calc_z0():
+    assert math.isclose(ABScorr.calc_z0(24,18,12,0.5,15),16.8)
+    assert math.isclose(ABScorr.calc_z0(24,12,10,0.75,12),22.5)
 
-# def calc_Asx(c:float, t:float, phi:float) -> float:
+def test_calc_Iy():
+    assert math.isclose(ABScorr.calc_Iy(24,28,18,12,0.5,5,16.8),1181.341666666667)
+    assert math.isclose(ABScorr.calc_Iy(24,24,12,10,0.75,4,22.5),376.6875)
 
+def test_calc_SM():
+    assert math.isclose(ABScorr.calc_SM(96,24,12),8.0)
+    assert math.isclose(ABScorr.calc_SM(40,20,11),3.6363636363636362)
 
-# def calc_z0(a:float, c:float, d:float, t:float, A:float) -> float:
+def test_calc_r():
+    assert math.isclose(ABScorr.calc_r(8,2),2.0)
+    assert math.isclose(ABScorr.calc_r(625,25),5.0)
 
+def test_calc_kc():
+    assert math.isclose(ABScorr.calc_kc(24,28,18),56.3062640625)
+    assert math.isclose(ABScorr.calc_kc(24,24,18),56.3062640625)
 
-# def calc_Iy(a:float, b:float, c:float, d:float, t:float, A:float, z0:float)  -> float:
+def test_calc_sigma_EC():
+    assert math.isclose(ABScorr.calc_sigma_EC(3e7,12,144),2056167.5835602828)
+    assert math.isclose(ABScorr.calc_sigma_EC(3e7,24,100),17054676.40508241)
 
+def test_calc_sigma_EB():
+    assert math.isclose(ABScorr.calc_sigma_EB(5,0.5,24,28,3e7,0.3),4380.186140390222)
+    assert math.isclose(ABScorr.calc_sigma_EB(4,0.75,24,24,3e7,0.3),10731.456043956045)
 
-# def calc_SM(Iy:float, d:float, z0:float) -> float:
+# def test_calc_UC_unit_corr():
 
+# def test_calc_Dx():
 
-# def calc_r(Iy:float, A:float) -> float:
+# def test_calc_Dy():
 
+# def test_calc_phi_x():
 
-# def calc_kc(a:float, b:float, c:float) -> float:
+# def test_calc_phi_y():
 
+# def test_calc_kx_cbhd():
 
-# def calc_sigma_EC(E:float, r:float, L:float) -> float:
+# def test_calc_ky_cbhd():
 
+# def test_calc_ks_cbhd():
 
-# def calc_sigma_EB(kc:float, t:float, a:float, b:float, E:float=30.0e6, nu:float=0.3) -> float:
+# def test_calc_sigma_Ex_cbhd():
 
+# def test_calc_sigma_Ey_cbhd():
 
-# def calc_UC_unit_corr(sigma_a:float, sigma_b:float, sigma_CA:float, sigma_CB:float, sigma_EC:float, eta:float) -> float:
+# def test_calc_tau_E_cbhd():
 
-
-# def calc_Dx(Iy:float, spc:float, E:float = 30.0e6) -> float:
-
-
-# def calc_Dy(a:float, b:float, c:float, t:float, spc:float, E:float, nu:float) -> float:
-
-
-# def calc_phi_x(L:float, B:float, Dx:float, Dy:float) -> float:
-
-
-# def calc_phi_y(L:float, B:float, Dx:float, Dy:float) -> float:
-
-
-# def calc_kx_cbhd(L:float, B:float, Dx:float, Dy:float, phi_x:float) -> float:
-
-
-# def calc_ky_cbhd(L:float, B:float, Dx:float, Dy:float, phi_y:float) -> float:
-
-
-# def calc_ks_cbhd() -> float:
-
-
-# def calc_sigma_Ex_cbhd(kx:float, Dx:float, Dy:float, tx:float, B:float) -> float:
-
-
-# def calc_sigma_Ey_cbhd(ky:float, Dx:float, Dy:float, t:float, L:float) -> float:
-
-
-# def calc_tau_E_cbhd(ks:float, Dx:float, Dy:float, t:float, L:float) -> float:
-
-
-# def calc_UC_cbhd(sigma_x: float, sigma_y: float, tau: float, sigma_Gx: float, sigma_Gy: float, tau_G: float, eta: float) -> float:
+# def test_calc_UC_cbhd():
